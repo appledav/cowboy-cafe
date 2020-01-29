aecc3eba76f581db313f455fd3b1193878dc78d7
David Apple <appledav@ksu.edu>
2020-01-29 14:18:03 -0600

    have to update visual studio

---
+++ Data/AngryChicken.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class AngryChicken
+    {
+
+        private bool _pickle = true;
+        private bool _bread = true;
+
+        /// <summary>
+        /// the price of the sandwich
+        /// </summary>
+        public double Price
+        {
+            get => 5.99;
+        }
+
+        /// <summary>
+        /// the number of calories
+        /// </summary>
+        public uint Calories
+        {
+            get => 190;
+        }
+
+        /// <summary>
+        /// if the sandwich has pickle
+        /// </summary>
+        public bool Pickle
+        {
+            get { return _pickle; }
+            set { _pickle = value; }
+        }
+
+        /// <summary>
+        /// if bread is to be used
+        /// </summary>
+        public bool Bread
+        {
+            get { return _bread; }
+            set { _bread = value; }
+        }
+
+        /// <summary>
+        /// The list of special instructions to make 
+        /// Angry Chicken
+        /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+                if (!_pickle) instructions.Add("Hold pickle");
+                if (!_bread) instructions.Add("Hold bread");
+
+                return instructions;
+            }
+        }
+    }
+}
---
+++ Data/RustlersRibs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class RustlersRibs
+    {
+        /// <summary>
+        /// the price of the ribs
+        /// </summary>
+        public double Price
+        {
+            get => 7.50;
+        }
+
+        /// <summary>
+        /// the number of calories
+        /// </summary>
+        public uint Calories
+        {
+            get => 894;
+        }
+
+         /// <summary>
+         /// The list of special instructions to make 
+         /// Rustler ribs
+         /// </summary>
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                var instructions = new List<string>();
+                instructions = null;
+                return instructions;
+            }
+        }
+    }
+}
